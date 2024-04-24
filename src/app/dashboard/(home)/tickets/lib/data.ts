0010dfd1e50eb609e07e85b92a5f6202d0c1b0f5
Bilal-Ghiffari <bilalalghiffari53@gmail.com>
2024-04-24 15:56:51 +0700

    Tickets - (slicing and integrate)

---
+++ src/app/dashboard/(home)/tickets/lib/data.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import prisma from "../../../../../../lib/prisma";
+
+export const getTickets = async () => {
+  try {
+    const data = await prisma.ticket.findMany({
+      include: {
+        customer: true,
+        flight: true,
+        seat: true,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log("error", error);
+    return [];
+  }
+};
