fcd8e2202d604c41bef5a72ee5950f39eb2be68d
Bilal-Ghiffari <bilalalghiffari53@gmail.com>
2024-04-25 16:40:12 +0700

    Landingpage:home (slicing)

---
+++ src/app/(landingpage)/(home)/page.tsx
@@ -0,0 +1,13 @@
+import BestSelective from "../components/organisms/best-selective";
+import Service from "../components/organisms/service";
+import Testimonials from "../components/organisms/testimonials";
+
+export default function Home() {
+  return (
+    <>
+      <Service />
+      <BestSelective />
+      <Testimonials />
+    </>
+  );
+}
